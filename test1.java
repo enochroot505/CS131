cc1b58da274e85490e11c621b926496c1e8f9f6d
enochroot505 <baumansc@gmail.com>
2024-09-17 09:13:58 -0700

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,12 @@
+package testbed;
+
+
+public class test1 {
+   public static void main(String[] args) {
+     int x = 5;
+     int z = x++;
+     int y = z + x;
+     System.out.println(y);
+
+   }
+}
